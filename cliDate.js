43747d71754b8cd3a32bfcf5096a3a384e6e0ac5
Александр Климов1~ <a.klimov@dazur-digital.com>
2023-06-20 01:23:14 +0300

    fix arguments in function

--- cliDate.js
+++ cliDate.js
@@ -1,36 +1,43 @@
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
+const argvArray = Object.keys(argv);
+const currentDate = new Date();
+// console.log(argv)
 
-const getCurrentDate = (currentDate, argvArray) => {
+const getCurrentDate = () => {
   if (argv["_"].length === 1 && argvArray.length === 2) {
     console.log(currentDate.toISOString());
   }
 };
 
-const getcurrentYear = (currentDate, argvArray) => {
+const getcurrentYear = () => {
   if (argvArray.includes("year") || argvArray.includes("y")) {
     console.log(currentDate.getFullYear());
   }
 };
 
-const getcurrentMonth = (currentDate, argvArray) => {
-  if (argvArray.includes("month") || argvArray.includes("m")) {
+const getcurrentMonth = () => {
+  if ((argvArray.includes("month") || argvArray.includes("m")) && argv["_"].length === 1) {
     console.log(currentDate.getMonth() + 1);
   }
 };
 
-const getcurrentDay = (currentDate, argvArray) => {
-  if (argvArray.includes("date") || argvArray.includes("d")) {
+const getcurrentDay = () => {
+  if ((argvArray.includes("date") || argvArray.includes("d")) && argv["_"].length === 1)  {
     console.log(currentDate.getDate());
   }
 };
 
-const predicateTwoArgs = (argvArray) => {
+const predicateTwoArgs = () => {
   return argvArray.length === 3 && argv["_"].length <= 2 ? true : false;
 };
 
-const getNextDate = (argvArray) => {
+const predicateOneCurrent = () => {
+    return argv["_"].length === 1 ? true : false;
+  };
+
+const getNextDate = () => {
   if (argv["_"][1] === "add" && argvArray.includes("d")) {
     const millisecondsDay = Date.now() + 86400000 * argv["d"];
     const isoString = new Date(millisecondsDay).toISOString();
@@ -38,7 +45,7 @@ const getNextDate = (argvArray) => {
   }
 };
 
-const getPrevDate = (argvArray) => {
+const getPrevDate = () => {
   if (argv["_"][1] === "sub" && argvArray.includes("month")) {
     const millisecondsDay = Date.now() - 86400000 * 30 * argv["month"];
     const isoString = new Date(millisecondsDay).toISOString();
@@ -47,16 +54,16 @@ const getPrevDate = (argvArray) => {
 };
 
 const mainGetDate = () => {
-  const currentDate = new Date();
-  const argvArray = Object.keys(argv);
   if (argv && argv["_"][0] === "current") {
-    getCurrentDate(currentDate, argvArray);
-    if (predicateTwoArgs(argvArray)) {
-      getcurrentYear(currentDate, argvArray);
-      getcurrentMonth(currentDate, argvArray);
-      getcurrentDay(currentDate, argvArray);
-      getNextDate(argvArray);
-      getPrevDate(argvArray);
+    getCurrentDate();
+    if (predicateTwoArgs()) {
+        if (predicateOneCurrent()) {
+            getcurrentYear();
+            getcurrentMonth();
+            getcurrentDay();
+        }
+      getNextDate();
+      getPrevDate();
     }
   }
 };
