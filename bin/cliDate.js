e5fe5997f8f37a21c4413a981546c54d5a845c11
Uqatebos <frozn00@mail.ru>
2023-08-13 04:41:25 +0300

    final version

--- bin/cliDate.js
+++ bin/cliDate.js
@@ -1,103 +1,40 @@
 #!/usr/bin/env node
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
-const { array } = require("yargs");
 const argv = yargs(hideBin(process.argv)).argv;
-const argvArray = Object.keys(argv);
-const currentDate = new Date();
-const year = argv?.y
-console.log(argv);
-console.log(argv["_"]);
-console.log(argvArray);
-console.log(year);
+const currentDate = argv["_"][0] === "current" && new Date();
+const currentYear = argv?.y || argv?.year;
+const currentMonth = argv?.m || argv?.month;
+const currentDay = argv?.d || argv?.date;
+const next = argv["_"][1] === "add" && argv?.d;
+const prev = argv["_"][1] === "sub" && argv?.month;
 
-// let d = new Date();
-// d.setSeconds(d.getSeconds() + 70);
-
-
-// let d = new Date();
-
-//  d.setMonth(d.getMonth() + 8);
-
-// console.log( d ); // 2 марта, 2011
-
-
-// const event = new Date('August 19, 1975 23:15:30');
-
-// event.setMonth(3);
-
-// console.log(event.getMonth());
-// // Expected output: 3
-
-// console.log(event);
-
-// const getCurrentDate = () => {
-//   if (argv["_"].length === 1 && argvArray.length === 2) {
-//     console.log(currentDate.toISOString());
-//   }
-// };
-
-// const getcurrentYear = () => {
-//   if (argvArray.includes("year") || argvArray.includes("y")) {
-//     console.log(currentDate.getFullYear());
-//   }
-// };
-
-// const getcurrentMonth = () => {
-//   if (
-//     (argvArray.includes("month") || argvArray.includes("m")) &&
-//     argv["_"].length === 1
-//   ) {
-//     console.log(currentDate.getMonth() + 1);
-//   }
-// };
-
-// const getcurrentDay = () => {
-//   if (
-//     (argvArray.includes("date") || argvArray.includes("d")) &&
-//     argv["_"].length === 1
-//   ) {
-//     console.log(currentDate.getDate());
-//   }
-// };
-
-// const predicateTwoArgs = () => {
-//   return argvArray.length === 3 && argv["_"].length <= 2 ? true : false;
-// };
-
-// const predicateOneCurrent = () => {
-//   return argv["_"].length === 1 ? true : false;
-// };
-
-// const getNextDate = () => {
-//   if (argv["_"][1] === "add" && argvArray.includes("d")) {
-//     const millisecondsDay = Date.now() + 86400000 * argv["d"];
-//     const isoString = new Date(millisecondsDay).toISOString();
-//     console.log(isoString);
-//   }
-// };
-
-// const getPrevDate = () => {
-//   if (argv["_"][1] === "sub" && argvArray.includes("month")) {
-//     const millisecondsDay = Date.now() - 86400000 * 30 * argv["month"];
-//     const isoString = new Date(millisecondsDay).toISOString();
-//     console.log(isoString);
-//   }
-// };
-
-// const mainGetDate = () => {
-//   if (argv && argv["_"][0] === "current") {
-//     getCurrentDate();
-//     if (predicateTwoArgs()) {
-//       if (predicateOneCurrent()) {
-//         getcurrentYear();
-//         getcurrentMonth();
-//         getcurrentDay();
-//       }
-//       getNextDate();
-//       getPrevDate();
-//     }
-//   }
-// };
-
-// mainGetDate();
+if (currentDate) {
+  if (next) {
+    currentDate.setDate(currentDate.getDate() + next);
+    console.log(
+      "Дата и время в формате ISO на " +
+        next +
+        " дня вперёд: " +
+        currentDate.toISOString()
+    );
+  } else if (prev) {
+    currentDate.setMonth(currentDate.getMonth() - prev);
+    console.log(
+      "Дата и время в формате ISO на  " +
+        prev +
+        " месяц назад: " +
+        currentDate.toISOString()
+    );
+  } else if (currentYear) {
+    console.log("Текущий год: " + currentDate.getFullYear());
+  } else if (currentMonth) {
+    console.log("Текущий месяц: " + (currentDate.getMonth() + 1));
+  } else if (currentDay) {
+    console.log("Дата в календарном месяце: " + currentDate.getDate());
+  } else {
+    console.log(
+      "Текущая дата и время в формате ISO: " + currentDate.toISOString()
+    );
+  }
+}
--- bin/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-
- d.setMonth(d.getMonth() + 8);
-
-console.log( d ); // 2 марта, 2011
